477e7d8fb55ba50887b82539e57b941316842d25
pctechkid <omboyronnel@gmail.com>
2020-12-21 02:12:46 +0800

    multiple fixes and improvements

--- js/validate.js
+++ js/validate.js
@@ -1,16 +1,36 @@
-function validate(){
+	function validate(){
 		var username=document.getElementById("username").value;
 		var password=document.getElementById("password").value;
 		if(username=="chua"&& password=="chuapremiumaccount")
 		{
-			alert("Logged in successfully!");
-			location.replace('dashboard.html');
+			document.getElementById("le-alert").classList.add('fade');
+			document.getElementById("le-alert").classList.remove('alert-danger');
+			document.getElementById("le-alert").classList.remove('alert-warning');
+			document.getElementById("le-alert").classList.add('alert-success');
+			document.getElementById("alertlabel").innerHTML = "Success!";
+			document.getElementById("alertsubtext").innerHTML = "Please wait, redirecting to dashboard.";
+			$('#le-alert').addClass('in');
 			return true;
-
 		}
 		else
 		{
-			alert("Incorrect username/password");
+			document.getElementById("le-alert").classList.add('fade');
+			document.getElementById("le-alert").classList.remove('alert-success');
+			document.getElementById("le-alert").classList.remove('alert-warning');
+			document.getElementById("le-alert").classList.add('alert-danger');
+			document.getElementById("alertlabel").innerHTML = "Error!";
+			document.getElementById("alertsubtext").innerHTML = "Invalid username/password.";
+			$('#le-alert').addClass('in');
 			return false;
 		}
-}
+	}
+	
+	$('.close').click(function () {
+      $(this).parent().removeClass('in');
+    });
+	
+	function proceed(){
+		setTimeout(function () {
+			location.replace('dashboard.html');
+		}, 2000);
+	}
